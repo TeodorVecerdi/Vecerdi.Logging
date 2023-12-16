347cfecedebf1a232bc72127ac40dd53b3583374
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-12-16 12:28:00 +0200

    Move Vecerdi.Logging to separate solution

---
+++ source/Vecerdi.Logging/ILogger.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+
+namespace Vecerdi.Logging;
+
+[PublicAPI]
+public interface ILogger {
+    /// <summary>
+    /// The minimum log level to log.
+    /// </summary>
+    LogLevel MinimumLogLevel { get; set; }
+
+    /// <summary>
+    /// Logs a message with the specified category, context and log level.
+    /// </summary>
+    /// <param name="message">The message to log.</param>
+    /// <param name="category">The category of the message.</param>
+    /// <param name="context">The context of the message.</param>
+    /// <param name="logLevel">The log level of the message.</param>
+    void Message(ReadOnlySpan<char> message, ReadOnlySpan<char> category = default, IContext? context = null, LogLevel logLevel = LogLevel.Information);
+
+    /// <summary>
+    /// Logs an exception with the specified category, context and log level.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="category">The category of the exception.</param>
+    /// <param name="context">The context of the exception.</param>
+    /// <param name="logLevel">The log level of the exception message.</param>
+    void Exception(Exception? exception, ReadOnlySpan<char> category = default, IContext? context = null, LogLevel logLevel = LogLevel.Error);
+}
