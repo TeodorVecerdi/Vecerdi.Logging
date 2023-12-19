6fab0c972bf91501bce4d791b297f19f61ef8d1d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-12-19 18:14:41 +0200

    Move LogCategoryAttribute into core library

---
+++ source/Vecerdi.Logging/LogCategoryAttribute.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace Vecerdi.Logging;
+
+[PublicAPI]
+[AttributeUsage(AttributeTargets.Field)]
+public class LogCategoryAttribute : Attribute {
+    public LogLevel DefaultLogLevel { get; } = LogLevel.Information;
+
+    public LogCategoryAttribute(LogLevel defaultLogLevel = LogLevel.Information) {
+        DefaultLogLevel = defaultLogLevel;
+    }
+}
