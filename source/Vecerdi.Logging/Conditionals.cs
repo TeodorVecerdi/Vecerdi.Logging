c2725910580fd733b5c8ab49e3b633b88d902391
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-12-20 08:59:56 +0200

    Update condition for enabling logging

--- source/Vecerdi.Logging/Conditionals.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vecerdi.Logging;
-
-internal static class Conditionals {
-#if NO_LOGGING
-    public const string ENABLE_LOGGING = "NO_LOGGING_8BB6260701404046B1BF269D15ED609A__7A1B24E50B684F1DBCCE57B02B82F233";
-#else
-    public const string ENABLE_LOGGING = "NETSTANDARD";
-#endif
-}
