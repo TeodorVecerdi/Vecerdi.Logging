c9da89aaec74d4ed6e020a30a98dcf74fd9cd9a4
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-12-20 09:56:42 +0200

    Move LoggingSettings asset location to Library folder

--- unity-integration/Vecerdi.Logging.Unity/Settings/CustomProjectSettings.cs
+++ unity-integration/Vecerdi.Logging.Unity/Settings/CustomProjectSettings.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 namespace Vecerdi.Logging.Unity {
     internal static class CustomProjectSettings {
         public static string ProjectPath { get; } = Directory.GetParent(Application.dataPath)!.FullName;
-        public static string ProjectSettingsPath { get; } = Path.Combine(ProjectPath, "ProjectSettings/Vecerdi.Logging");
+        public static string ProjectSettingsPath { get; } = Path.Combine(ProjectPath, "Library/Vecerdi.Logging");
     }
 }
