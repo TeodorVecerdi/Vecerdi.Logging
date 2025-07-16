d2e22310af5c198dd5b45e5e98050721980448f8
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-07-16 17:20:40 +0300

    Add addressables support

---
+++ unity-integration/Vecerdi.Logging.Unity/AddressablesSupport/LoggingBuildScriptPackedMode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Build.DataBuilders;
+using UnityEngine;
+using Vecerdi.Logging.Unity.Editor.Settings;
+
+namespace Vecerdi.Logging.Unity.AddressablesSupport {
+    [CreateAssetMenu(fileName = "LoggingBuildScriptPacked", menuName = "Addressables/Custom Content Builders/Packed Mode (with Logging CodeGen)")]
+    public sealed class LoggingBuildScriptPackedMode : BuildScriptPackedMode {
+        public override string Name => "Default Build Script (with Logging CodeGen)";
+
+        protected override TResult BuildDataImplementation<TResult>(AddressablesDataBuilderInput context) {
+            LoggingSettingsCodeGenerator.GenerateEmbeddedConfiguration();
+            try {
+                return base.BuildDataImplementation<TResult>(context);
+            } finally {
+                LoggingSettingsCodeGenerator.RemoveEmbeddedConfiguration();
+            }
+        }
+    }
+}
